42b1f8d1154c516d9343edd3b7e4d32654cc9c09
Joe Giuffrida <jm.giuffrida@gmail.com>
2016-11-22 19:38:48 -0500

    add more things to listen to

--- bot/index.js
+++ bot/index.js
@@ -3,7 +3,7 @@
 
     module.exports = {
         init: function (controller, bot) {
-            controller.hears(/where('s|s)? (is )?xur\??/, () => {
+            controller.hears(/where('s|s)? (is )?xur\??/, ['direct_message','direct_mention','mention'], () => {
                 bot.reply('I don\'t know, probably in the reef? Yes, try there.');
             });
         }
