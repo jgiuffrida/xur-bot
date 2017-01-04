6c92aeca5cdd95d37c156850dc62b02c81e82d3e
Joe Giuffrida <jm.giuffrida@gmail.com>
2017-01-03 21:58:29 -0500

    add pizza calc

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const process = require('process');
+const skellington = require('skellington');
 
 if (process.env.SLACK_TOKEN) {
-    require('skellington')({
+    skellington({
         slackToken: process.env.SLACK_TOKEN,
-        plugins: [require('./bot/index')],
+        plugins: [require('./bot/index'), require('pizza-calculator/bot')],
         debug: false
     });
+
+    require
 } else {
     process.exit(1);
 }
+
+skellington({
+    slackToken: process.env.CCJS_TOKEN,
+    plugins: [require('pizza-calculator/bot')],
+    debug: false
+});
