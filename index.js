8f972f0e3e79a316ccb64348e42a0929903f4607
Joe Giuffrida <jm.giuffrida@gmail.com>
2017-01-03 22:42:33 -0500

    add check for ccjs token

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ if (process.env.SLACK_TOKEN) {
         plugins: [require('./bot/index'), require('pizza-calculator/bot')],
         debug: false
     });
-
-    require
 } else {
     process.exit(1);
 }
 
-skellington({
-    slackToken: process.env.CCJS_TOKEN,
-    plugins: [require('pizza-calculator/bot')],
-    debug: false
-});
+if(process.env.CCJS_TOKEN) {
+    skellington({
+        slackToken: process.env.CCJS_TOKEN,
+        plugins: [require('pizza-calculator/bot')],
+        debug: false
+    });
+}
